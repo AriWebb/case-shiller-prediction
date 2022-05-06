d75fd6a4b018731068ab4aa4f1d97df5289720ab
matt-wolff <mbwolff@stanford.edu>
2022-05-06 14:17:47 -0700

    Added stock data to all_data_w_city_names.

--- data/combine_processed.py
+++ data/combine_processed.py
@@ -4,6 +4,8 @@ def main():
     all_data = {}
     city_files = ['cpi/cpi.csv', 'crime/crime.csv', 'Patents_processed.csv',
                   'Population_processed.csv', 'Case_Shiller_processed.csv']
+    stock_files = ['Processed_Stock_Data/DOW_processed.csv', 'Processed_Stock_Data/NASDAQ_processed.csv',
+                   'Processed_Stock_Data/SP_processed.csv']
     for file in city_files:
         first_row = True
         for row in open(file):
@@ -13,16 +15,23 @@ def main():
                 row = row.strip().split(',')
                 key = row[0] + ',' + row[1]
                 if key not in all_data:
-                    all_data[key] = [float(value) for value in row[2:]]
+                    all_data[key] = [value for value in row[2:]]
                 else:
                     for value in row[2:]:
-                        all_data[key].append(float(value))
+                        all_data[key].append(value)
 
     num_features = max([len(lst)] for lst in all_data.values())[0]
     full_data = {}  # Dictionary with data points that have all features
     for key in all_data:
         if len(all_data[key]) == num_features:
             full_data[key] = all_data[key]
+    for key in full_data:
+        date = key.split(',')[1]
+        for file in stock_files:
+            for row in open(file):
+                if row.strip().split(',')[0] == date:
+                    full_data[key].append(row.strip().split(',')[1])
+                    break
     f = open('all_data_w_city_names.csv', 'w')
     writer = csv.writer(f)
     for key in full_data:
