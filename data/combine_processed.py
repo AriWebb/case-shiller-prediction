7085ba51e5f1517ab22586873a46225c91a42260
matt-wolff <mbwolff@stanford.edu>
2022-05-06 14:43:09 -0700

    Added labels to all_data.

--- data/combine_processed.py
+++ data/combine_processed.py
@@ -1,7 +1,7 @@
 import csv
 
 LABELS = ['city', 'date', 'cpi', 'crimes_reported', 'crimes_cleared', 'patents', 'population', 'case_shiller', 'dow',
-          'nasdaq', 'sp']
+          'nasdaq', 'sp', 'label']
 
 def main():
     all_data = {}
@@ -9,6 +9,15 @@ def main():
                   'Population_processed.csv', 'Case_Shiller_processed.csv']
     stock_files = ['Processed_Stock_Data/DOW_processed.csv', 'Processed_Stock_Data/NASDAQ_processed.csv',
                    'Processed_Stock_Data/SP_processed.csv']
+
+    # Create Case_Shiller dictionary
+    case_shiller_dict = {}
+    for row in open('Case_Shiller_processed.csv'):
+        row = row.strip().split(',')
+        city_date = row[0] + ',' + row[1]
+        case_shiller_dict[city_date] = row[2]
+
+    # Combine all city data
     for file in city_files:
         first_row = True
         for row in open(file):
@@ -23,11 +32,25 @@ def main():
                     for value in row[2:]:
                         all_data[key].append(value)
 
+    # Keep examples with all features and label
     num_features = max([len(lst)] for lst in all_data.values())[0]
     full_data = {}  # Dictionary with data points that have all features
     for key in all_data:
-        if len(all_data[key]) == num_features:
+        city = key.split(',')[0]
+        future_month_str = key.split(',')[1][:3]
+        future_year_int = int(key.split(',')[1][3:]) + 5
+        if future_year_int >= 100:
+            future_year_str = str(future_year_int)[1:]
+        else:
+            future_year_str = str(future_year_int)
+        future_date = future_month_str + future_year_str
+        # If we have the label and all the features
+        future_key = city + ',' + future_date
+        if future_key in case_shiller_dict.keys() and len(all_data[key]) == num_features:
             full_data[key] = all_data[key]
+            full_data[key].append(case_shiller_dict[future_key])
+
+    # Add stock data
     for key in full_data:
         date = key.split(',')[1]
         for file in stock_files:
@@ -35,6 +58,8 @@ def main():
                 if row.strip().split(',')[0] == date:
                     full_data[key].append(row.strip().split(',')[1])
                     break
+
+    # Write to csv
     f = open('all_data_w_city_names.csv', 'w')
     writer = csv.writer(f)
     writer.writerow(LABELS)
